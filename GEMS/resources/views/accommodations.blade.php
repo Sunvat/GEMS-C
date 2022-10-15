c580f1465d23a1e663b157fe140c4b8b7f71303c
Mack S <60047062+Time-Vault@users.noreply.github.com>
2022-10-14 18:45:46 -0700

    Update accommodations.blade.php

--- GEMS/resources/views/accommodations.blade.php
+++ GEMS/resources/views/accommodations.blade.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 </br></br></br>
+TODO: Make this page put content out of the way of the header automatically
 <b>
 This is a test. Accommodations will not be accessed from the homepage directly.
 </b>

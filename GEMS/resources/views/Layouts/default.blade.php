8b916c685ea6af4fd583db678bbc80dd53abb5dd
Mack S <60047062+Time-Vault@users.noreply.github.com>
2022-10-14 18:10:19 -0700

    Fixed a typo

--- GEMS/resources/views/Layouts/default.blade.php
+++ GEMS/resources/views/Layouts/default.blade.php
@@ -12,7 +12,7 @@
      <header class = "fixed bg-gray-400 top-0 left-5 right-0 z-50">
         <div class="container mx-auto">
             <h1 class="text-xl font-black">
-                Glohaven Emergancy Management System
+                Glohaven Emergency Management System
             </h1>
             <nav class="-mx-1">
                 <a href="{{route('Home')}}" class="text-lg mx-2 text-white hover:text-orange-400 transition">Home</a>

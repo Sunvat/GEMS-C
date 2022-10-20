841f0568066ebdbfc55cf022399b938753ba2901
Mack S <60047062+Time-Vault@users.noreply.github.com>
2022-10-20 11:51:12 -0700

    Restored the default layout

--- GEMS/resources/views/Layouts/default.blade.php
+++ GEMS/resources/views/Layouts/default.blade.php
@@ -6,19 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Glohaven Emergency Management System</title>
     @vite('resources/css/app.css')
- 
-</head>
 
-     
+</head>
+<body class = "bg-gray-400" >
+     <header class = "fixed bg-gray-400 top-0 left-5 right-0 z-50">
+        <div class="container mx-auto">
+            <h1 class="text-xl font-black">
+                Glohaven Emergency Management System
+            </h1>
+            <nav class="-mx-1">
+                <a href="{{route('Home')}}" class="text-lg mx-2 text-white hover:text-orange-400 transition">Home</a>
+                <a href="{{route('Regions')}}" class="text-lg mx-2 text-white hover:text-orange-400 transition">Regions</a>
+                <a href="{{route('Log-In')}}" class="text-lg mx-2 text-white hover:text-orange-400 transition">Log in</a>
+                <a href="{{route('Accommodations')}}" class="text-lg mx-2 text-white hover:text-orange-400 transition">Accommodations</a>
+            </nav>
+
+    </div>
+     </header>
      <main>
         @yield('main')
      </main>
-
-
      <footer>
-        <p>Footer</p>
+        
      </footer>
 </body>
 </html>
-
-

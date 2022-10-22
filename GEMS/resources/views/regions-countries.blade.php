132dc2e24fc1137a01ca062556a074f3e132cb8f
Sunvat <sunvat14@gmail.com>
2022-10-21 20:44:34 -0700

    changes in the region page added syntax.

--- GEMS/resources/views/regions-countries.blade.php
+++ GEMS/resources/views/regions-countries.blade.php
@@ -1,5 +1,14 @@
 @extends('Layouts.default')
 
-@section('default')
+@section('main')
+<!DOCTYPE html>
+<html>
+<body class = "">
+   
+This page is the regions page where the EMS can select regions from
+   
+</body>
+</html>
+
 homepage
 @endsection

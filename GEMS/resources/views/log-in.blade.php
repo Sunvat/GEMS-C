2b6430e1f56583bd4c42a5ccf1399e2a241f4ba0
Hark25 <harklit25@gmail.com>
2022-10-26 10:01:50 -0700

    deleted booking route and page

--- GEMS/resources/views/log-in.blade.php
+++ GEMS/resources/views/log-in.blade.php
@@ -1,7 +1,11 @@
 @extends('Layouts.default')
 
 @section('main')
- <h1>
-    
- </h1>
+ <form method ="post">
+    <label for="id">Login ID</label>
+    <input type="text" id = "id" name = "id">
+    <label for="Password">Password</label>
+    <input type="text" id = "id" name = "id">
+    <input type="submit" value = "Login">
+ </form>
 @endsection

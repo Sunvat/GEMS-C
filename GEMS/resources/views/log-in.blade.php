78ee3176383de6a85d853a2fd77f5c0c8f480ab9
Hark25 <harklit25@gmail.com>
2022-10-26 12:25:59 -0700

    added back booking and changes to login

--- GEMS/resources/views/log-in.blade.php
+++ GEMS/resources/views/log-in.blade.php
@@ -1,11 +1,15 @@
 @extends('Layouts.default')
 
 @section('main')
- <form method ="post">
-    <label for="id">Login ID</label>
-    <input type="text" id = "id" name = "id">
-    <label for="Password">Password</label>
-    <input type="text" id = "id" name = "id">
-    <input type="submit" value = "Login">
- </form>
+<div class = "flex flex-col justify-center items-center">
+    <form> <!-- dont forget to add a post or someother method when needed -->
+        <fieldset>
+            <label for="id" class = "px-2">Login ID</label>
+            <input type="text" id = "id" name = "id" class="px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"><br>  
+            <label for="Password" class = "px-1 ">Password</label>
+            <input type="text" id = "id" name = "id" class = "px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"><br>
+        </fieldset>
+        <input type="submit" value = "Login"><br>
+    </form>
+</div>
 @endsection

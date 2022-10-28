5c875f73981f4715ae8a3d5a4b7514fc2f57a1d2
Hark25 <harklit25@gmail.com>
2022-10-28 10:51:44 -0700

    added some information and divides

--- GEMS/resources/views/Contact.blade.php
+++ GEMS/resources/views/Contact.blade.php
@@ -1,6 +1,24 @@
 @extends('Layouts.default')
+
 @section('main')
-<b>
-this page is for contact info, this will be used in the footer in defualt. will provide contac infothogh email and such.
-</b>
+<br>
+<div class = "flex flex-col justify-center items-center">
+    <div>
+        <h1>
+           Contact Information:
+        </h1>
+    </div>
+    <div>
+        <h2>
+            Email:
+        </h2>
+    </div>
+    <div>
+        <h2>
+            Call Us:
+        </h2>
+        <h3>Local Reservation Agent: (xxx) xxx xxxx</h3>
+        <h3>Support: (xxx) xxx xxxx</h3>
+    </div>
+</div>
 @endsection

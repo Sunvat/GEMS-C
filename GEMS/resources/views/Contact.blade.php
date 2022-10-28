18885ea9bb51436d1a600c30871c4c97456adc74
Hark25 <harklit25@gmail.com>
2022-10-28 11:14:02 -0700

    added css for contact page, please make it look better

--- GEMS/resources/views/Contact.blade.php
+++ GEMS/resources/views/Contact.blade.php
@@ -3,22 +3,26 @@
 @section('main')
 <br>
 <div class = "flex flex-col justify-center items-center">
-    <div>
-        <h1>
+    <div class = "p-1 bg-orange-400 rounded-lg">
+        <h1 class = "text-4xl text-slate-700">
            Contact Information:
         </h1>
     </div>
-    <div>
-        <h2>
+    <br>
+    <div class = "p-1 bg-orange-400 rounded-lg">
+        <h2 class = "text-3xl text-slate-700">
             Email:
         </h2>
+        <h3 class = "text-xl">For New Accomodations Information: xxxx@xxxx.xxx</h3>
+        <h3 class = "text-xl">For new Region Information: xxxx@xxxx.xxx</h3>
     </div>
-    <div>
-        <h2>
+    <br>
+    <div class = "p-1 bg-orange-400 rounded-lg">
+        <h2 class = "text-3xl text-slate-700">
             Call Us:
         </h2>
-        <h3>Local Reservation Agent: (xxx) xxx xxxx</h3>
-        <h3>Support: (xxx) xxx xxxx</h3>
+        <h3 class = "text-xl">Local Reservation Agent: (xxx) xxx xxxx</h3>
+        <h3 class = "text-xl">Support: (xxx) xxx xxxx</h3>
     </div>
 </div>
 @endsection

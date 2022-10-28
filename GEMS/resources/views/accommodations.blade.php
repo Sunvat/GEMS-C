06679ca6a452bf07aec82977839e65ef996f0fb2
Mack S <60047062+Time-Vault@users.noreply.github.com>
2022-10-28 09:44:00 -0700

    Updated style of buttons

--- GEMS/resources/views/accommodations.blade.php
+++ GEMS/resources/views/accommodations.blade.php
@@ -14,9 +14,7 @@ This is a test. Accommodations will not be accessed from the homepage directly.
 <?php if (true) : ?>
   <div class="container">
         <a href="/New">
-            <span id="create-accommodation-button"
-            style="background-color:#EC925D;color:#FFFFFF;padding-left:5px;padding-right:5px;">
-            New Accommodation</span>
+        <button type="button" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohave-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">New Accommodation</button>
         </a>
     </div>
 <?php endif; ?>
@@ -39,7 +37,7 @@ TODO: This should be auto populated once we have a database set up
     <td id="locColumn">City Hall</td>
     <td id="capColumn">20 people</td>
     <td id="buttonColumn"><a href="/View-Accommodation">
-      <button type="button" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohave-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">View</button>
+      <button type="button" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohaven-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">View</button>
       </a></td>
   </tr>
 </table>

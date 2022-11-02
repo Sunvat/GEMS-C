39b18f63447ad2d1eb9e83e46d7116dd0b53a9ef
Mack S <60047062+Time-Vault@users.noreply.github.com>
2022-11-02 10:45:37 -0700

    Added Form to Update Accommodation

--- GEMS/resources/views/update-accommodations.blade.php
+++ GEMS/resources/views/update-accommodations.blade.php
@@ -7,15 +7,22 @@
 </head>
 <body>
 <b>
-This is a test. Accommodations will not be accessed from the homepage directly.
+This is a test. Accommodations will not be accessed from the homepage directly.</br>
+TODO: Create function to change page after submitting
 </b></br></br>
-<div class="container">
-        <a href="/View-Accommodation">
-            <span id="view-accommodation-button"
-            style="background-color:#EC925D;color:#FFFFFF;padding-left:5px;padding-right:5px;">
-            Submit</span>
-        </a>
-    </div>
+<div class="container py-10 px-10 mx-0 min-w-full grid place-items-center" id = "container">
+<form action="/action_page.php" method="get" id="updateForm">
+  <label for="locName">Location Name:</label><br>
+  <input type="text" id="locName" name="locName" value="Placeholder"><br>
+  <label for="maxCap">Maximum Capacity:</label><br>
+  <input type="text" id="maxCap" name="maxCap" value="100"><br>
+  <label for="popChange">Net Change in Occupancy:</label><br>
+  <input type="text" id="popChange" name="popChange" value="0"><br>
+  <label for="desc">Description:</label><br>
+  <input type="text" id="desc" name="desc" value="Placeholder"><br><br>
+</form>
+<button type="submit" form="updateForm" value="Submit" onclick="location.href='/View-Accommodation';" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohave-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">Submit</button>
+</div>
 
 </body>
 @endsection

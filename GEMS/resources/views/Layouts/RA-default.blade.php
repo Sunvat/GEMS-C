2680ea1855397772853d365f0d6fd6a399cad2f8
Akshaj Srinivasan <akshajsrinivasan99@gmail.com>
2022-11-16 18:29:19 -0800

    EDITED FOOTER, MEETING Notes

--- GEMS/resources/views/Layouts/RA-default.blade.php
+++ GEMS/resources/views/Layouts/RA-default.blade.php
@@ -39,6 +39,34 @@
      <main>
         @yield('main')
      </main>
-     
+     <br>
+     <br>
+
+     <footer class="p-8 bg-white shadow md:px-6 md:py-3 dark:bg-orange-300 flex flex-col h-min">
+    <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="{{route('Home')}}" class="flex items-center mb-4 sm:mb-0">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvBCu8PJ1o41yD0tynwPfEO4sJ9qzu9igB85bzeOjnxQ&s" class="mr-3 h-8" alt="Glohaven Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Glohaven Emergency Management Solutions</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-700">
+            <li>
+                <a href="{{route('Home')}}" class="mr-4 hover:underline md:mr-6 ">About</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="#" class="mr-4 hover:underline md:mr-6 ">Licensing</a>
+            </li>
+            <li>
+                <a href="{{route('Contact')}}" class="hover:underline">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <p class="text-l italic text-gray-700 ">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; You cannot properly manage that, which you don't know you have.</p>
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-700">© 2022 <a href="{{route('Home')}}" class="hover:underline">Glohaven™</a>. All Rights Reserved.
+    </span>
+</footer>
 </body>
 </html>

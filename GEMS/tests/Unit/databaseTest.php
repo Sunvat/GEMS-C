a53d4c07dd825cd6d4e88ea9aaa9cdd808161507
Mack S <60047062+Time-Vault@users.noreply.github.com>
2022-11-23 12:07:53 -0800

    Created test for database insert. Select in progress.

---
+++ GEMS/tests/Unit/databaseTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+use vendor;
+
+class databaseTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function test_Insert()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "gems";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            $this->assertTrue(false);
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO accommodations (name, address, maxCap)
+        VALUES ('UBCO LIB 305', 'Test Street', '50')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+            $conn->close();
+            $this->assertTrue(true);
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            $conn->close();
+            $this->assertTrue(false);
+        }
+    }
+    public function test_Select()
+    {
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "gems";
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            $this->assertTrue(false);
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "SELECT name FROM accommodations WHERE name='UBCO LIB 305';";
+        $result = mysqli_query($conn,$sql);
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+        if (strcmp($rows[0], "UBCO LIB 305") == 0) {
+            $conn->close();
+            $this->assertTrue(true);
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+            $conn->close();
+            $this->assertTrue(false);
+        }
+    }
+}

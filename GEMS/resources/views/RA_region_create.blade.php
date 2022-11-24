352b37ee43be44857abc7b57184e18951a22b80b
Sunvat <sunvat14@gmail.com>
2022-11-23 23:43:45 -0800

    Created 2 new webpages for RA.

---
+++ GEMS/resources/views/RA_region_create.blade.php
@@ -0,0 +1,25 @@
+@extends('Layouts.default')
+@section('main')
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="app.css">
+</head>
+<body>
+<b>
+Logged in as RA.
+</b>
+<div class="container py-10 px-10 mx-0 min-w-full grid place-items-center" id = "container">
+<form action="/action_page.php" method="get" id="updateForm">
+  <label for="locName">Location Name:</label><br>
+  <input type="text" id="locName" name="locName" value="Placeholder"><br>
+  <label for="maxCap">Maximum Capacity:</label><br>
+  <input type="text" id="maxCap" name="maxCap" value="100"><br>
+  <label for="desc">Description:</label><br>
+  <input type="text" id="desc" name="desc" value="Placeholder"><br><br>
+</form>
+<button type="submit" form="updateForm" value="Submit" onclick="location.href='/View-Accommodation';" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohave-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">Submit</button>
+</div>
+
+</body>
+@endsection

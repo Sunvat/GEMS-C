d115b9adc046611e87670664d06989e1993b89bb
Hark25 <harklit25@gmail.com>
2022-11-25 11:23:09 -0800

    changed accomodations, due to Css changes, Latest info page done

--- GEMS/resources/views/Latest-Emergency-Updates.blade.php
+++ GEMS/resources/views/Latest-Emergency-Updates.blade.php
@@ -7,16 +7,21 @@
          Latest Emergancy Information
       </h1>
       <br>
-      <table class = "table-fixed border">
+      <table class = "table-fixed border mx-24">
          <tr class = "border border-slate-500">
             <th class = "border border-slate-500">Region</th>
             <th class = "bordor border-slate-500">City</th>
             <th class = "border border-slate-500">Update</th>
          </tr>
          <tr>
-            <th class = "border p-2">Okanagan</th>
-            <th class = "border p-2">Kelowna</th>
-            <th class = "border p-2">Massive floodings along Okanagan Lake all residents living within 100 kilometers are to be evacuated while those within 100-200 kilometers are to prepare for possible evacuation.</th>
+            <th class = "border p-2 border-slate-500">Okanagan</th>
+            <th class = "border p-2 border-slate-500">Kelowna</th>
+            <th class = "border p-2 border-slate-500">Massive floodings along Okanagan Lake all residents living within 100 meters are to be evacuated while those within 100-200 meters are to prepare for possible evacuation.</th>
+         </tr>
+         <tr>
+            <th class = "border p-2 border-slate-500">Vancouver Island</th>
+            <th class = "border p-2 border-slate-500">Victoria</th>
+            <th class = "border p-2 border-slate-500">Huge fires burning near victoria, residents on the northside are to prepare for evacutation to mainland.</th>
          </tr>
       </table>
    </div>

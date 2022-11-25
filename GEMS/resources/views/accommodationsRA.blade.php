755f05e29658dccc3e0569974a71f841552d13cc
Sunvat <sunvat14@gmail.com>
2022-11-25 13:25:30 -0800

    Final routes to RA pages

--- GEMS/resources/views/accommodationsRA.blade.php
+++ GEMS/resources/views/accommodationsRA.blade.php
@@ -11,9 +11,11 @@
 This is a test. Accommodations will not be accessed from the homepage directly.
 </b></br>
   <div class="container">
-        <a href="{{route('RA_region_create')}}">
-        <button type="button" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohave-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">New Accommodation</button>
+  <button type="button" class="focus:outline-none text-white bg-Glohaven-Orange hover:bg-Glohaven-Hovered focus:ring-4 focus:ring-Glohave-Orange font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:focus:ring-Glohaven-Hovered">
+        <a href="{{route('View-Accommodation-RA')}}">
+        New Accommodation
         </a>
+        </button>
     </div>
 
 </br>

5dd6d9ad3dadc1bcfaf9fd5b3bf54884b2ff34a4
Mack S <60047062+Time-Vault@users.noreply.github.com>
2023-01-13 10:53:13 -0800

    Restructured testing

--- GEMS/tests/Feature/insertTest.php
+++ GEMS/tests/Feature/insertTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use vendor;
-require('database/getConnection.php');
+require('database/insertFuncs.php');
 
 class databaseTest extends TestCase
 {
@@ -13,19 +13,13 @@ class databaseTest extends TestCase
      */
     public function test_Insert()
     {
-        // Create connection
-        $conn = getConn();
-
-        $sql = "INSERT INTO accommodations (aname, address, rname, country, maxCap)
-        VALUES ('UBCO LIB 305', 'Test Street', 'Okanagan', 'Canada', '50')";
-
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-            $conn->close();
+        $success = insertTestAcc();
+        
+        if ($success == TRUE) {
+            echo "New record created successfully.";
             $this->assertTrue(true);
         } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-            $conn->close();
+            echo "Failed to create record.";
             $this->assertTrue(false);
         }
     }

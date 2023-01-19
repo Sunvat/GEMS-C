e3ad3b6d1167eae43f2ea6df2f8819ce65ccd1e7
Mack S <60047062+Time-Vault@users.noreply.github.com>
2023-01-19 15:29:12 -0800

    Added delete test

--- GEMS/tests/Feature/databaseTest.php
+++ GEMS/tests/Feature/databaseTest.php
@@ -8,32 +8,33 @@ require('database/insertFuncs.php');
 require('database/selectFuncs.php');
 require('database/deleteFuncs.php');
 
-//Used in the delete test
-$ID = 0;
-
 class databaseTest extends TestCase
 {
     /**
      * @return void
      */
+
+    //Used in the delete test
+    public int $ID;
+    
     public function test_Insert()
     {
         $success = insertTestAcc();
         
         if ($success == TRUE) {
-            echo "New record created successfully.";
+            echo "New record created successfully.\n";
             $this->assertTrue(true);
         } else {
-            echo "Failed to create record.";
+            echo "Failed to create record.\n";
             $this->assertTrue(false);
         }
     }
+
     public function test_Select()
     {
         //Get Results
         $result = getAllAcc("TESTING");
         $rows = $result->fetch_row();
-        $ID = $rows[0];
 
         //Results exist
         if (strcmp($rows[1], "Hotel California") == 0 && strcmp($rows[2], "Dark Desert Highway") == 0) {
@@ -47,22 +48,17 @@ class databaseTest extends TestCase
 
     public function test_Delete()
     {
-        // Create connection
-        $conn = getConn();
-
-        //Get Results
+        //Jury rigged method of getting ID number
+        //In place until I can figure out why PHP won't let me change the class variable $ID
         $result = getAllAcc("TESTING");
         $rows = $result->fetch_row();
-        $ID = $rows[0];
-
-        //Results exist
-        if (strcmp($rows[1], "Hotel California") == 0 && strcmp($rows[2], "Dark Desert Highway") == 0) {
-            $conn->close();
+        $success = delAccTest($rows[0]);
+        
+        if ($success == TRUE) {
+            echo "Deleted $rows[0] Successfully.\n";
             $this->assertTrue(true);
-        //Results don't exist
         } else {
-            echo "Error: Could not find requested data.\nInstead retrieved $rows[1] at $rows[2].\n" . $conn->error;
-            $conn->close();
+            echo "Failed to delete record.\n";
             $this->assertTrue(false);
         }
     }

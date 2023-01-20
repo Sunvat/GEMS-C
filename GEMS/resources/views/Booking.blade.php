5d506c81c14aa33e92791e71f810f24c7743abee
Sunvat <sunvat14@gmail.com>
2023-01-19 18:16:47 -0800

    changes in bookings page

--- GEMS/resources/views/Booking.blade.php
+++ GEMS/resources/views/Booking.blade.php
@@ -17,38 +17,38 @@
             <form class="pb-1">
                  <fieldset class="border 5px px-5 py-5">
                     <legend class="mb-5 text-xl font-bold text-white">Request Booking.</legend>
-                    <label for="contactname">Caller First Name:</label>
+                    <label for="contactname" class=" text-white">Caller First Name:</label>
                      <input type="text" class = "border solid 5px  mb-2 display block ">
                      <br>
-                     <label for="contactname">Caller Last Name:</label>
+                     <label for="contactname"class=" text-white">Caller Last Name:</label>
                      <input type="text" class = "border solid 5px  mb-2 display block ">
                      <br>
-                      <label for="ems_division">Caller EMS Division:</label>
+                      <label for="ems_division"class=" text-white">Caller EMS Division:</label>
                       <input type="text" class = "border solid 5px; mb-2; display block ">
                      <br>
-                     <label for="I.D Number">Caller I.D Number:</label>
+                     <label for="I.D Number"class=" text-white">Caller I.D Number:</label>
                      <input type="text" class = "border solid 5px; mb-2; display block ">
                      <br>
-                     <label for="Email_Address">Caller Email Address:</label>
+                     <label for="Email_Address"class=" text-white">Caller Email Address:</label>
                      <input type="text" class = "border solid 5px; mb-2; display block ">
                      <br>
-                     <label for="CallerContactNumber:">Caller Contact Number:</label>
+                     <label for="CallerContactNumber:"class=" text-white">Caller Contact Number:</label>
                      <input type="text" class = "border solid 5px; mb-2; display block ">
                      <br>
-                     <label for="evacuee_nos">Number of Evacuees:</label>
+                     <label for="evacuee_nos"class=" text-white">Number of Evacuees:</label>
                      <input type="text" class = "border solid 5px; mb-2; display block ">
                      <br>
-                     <label for="PrimaryEvacueName">Primary Evacuee First Name:</label>
+                     <label for="PrimaryEvacueName"class=" text-white">Primary Evacuee First Name:</label>
                      <input type="text" class = "border solid 5px  mb-2 display block ">
                      <br>
-                     <label for="EvacueeContactNumber">Primary Evacuee Contact Number:</label>
+                     <label for="EvacueeContactNumber"class=" text-white">Primary Evacuee Contact Number:</label>
                      <input type="text" class = "border solid 5px; mb-2; display block ">
                  </fieldset>
                 </form>
                 <br>
                 <br>
                 <div class="inline-flex rounded-md shadow-sm" role="group">
-                <button type="button" onclick="location.href='/Booking-Submitted';" class="py-2 px-4 text-sm font-medium text-white bg-Glohaven-Orange rounded-lg border border-Glohaven-Orange hover:bg-Glohaven-Hovered hover:text-white focus:z-10 focus:ring-2 focus:ring-Glohaven-Orange focus:text-Glohaven-Orange dark:bg-Glohaven-Orange dark:border-Glohaven-Hovered dark:text-white dark:hover:text-white dark:hover:bg-Glohaven-Hovered dark:focus:Glohaven-Orange dark:focus:text-white">Submit</button>
+                <button type="button" onclick="location.href='/Booking-Submitted';" class="py-2 px-4 text-sm font-medium text-white bg-Dgreen rounded-lg border-2 border-gold ">Submit</button>
                 </div>
         </div>
         

795cba2adf563baf03726c2d96da6456c3748dfb
Hark25 <harklit25@gmail.com>
2023-01-25 14:31:26 -0800

    created insertLEI function, small changes to LEI-create page

--- GEMS/resources/views/LEI-Create-RA.blade.php
+++ GEMS/resources/views/LEI-Create-RA.blade.php
@@ -7,9 +7,11 @@ $rID = $_GET["rID"];
 
 <div class="container py-10 px-150 mx-0 min-w-full grid place-items-center" id = "container">
 <form action="" method="get" id="updateForm">
- <?php
-  echo "<input type=\"hidden\" id = \"rID\" name = \"rID\" value = \"$rID\">";
+
+  <?php
+    echo "<input type=\"hidden\" id = \"rID\" name = \"rID\" value = \"$rID\">";
   ?>
+
   <label for="Location" class="mx-2 font-bold text-slate-700">Location Name:</label><br>
   <input type="text" id="Location" name="Location" placeholder="Kelowna" class="rounded p-2"><br>
 
@@ -24,7 +26,7 @@ $rID = $_GET["rID"];
     <div class="md:w-1/3">
     </div>
     <div class="md:w-2/3">
-      <input type="submit" name="submit" value="submit" class="py-2 px-4 text-sm font-medium text-white bg-Glohaven-Orange rounded-lg border border-Glohaven-Orange hover:bg-Glohaven-Hovered hover:text-white focus:z-10 focus:ring-2 focus:ring-Glohaven-Orange focus:text-Glohaven-Orange dark:bg-Glohaven-Orange dark:border-Glohaven-Hovered dark:text-white dark:hover:text-white dark:hover:bg-Glohaven-Hovered dark:focus:ring-blue-500 dark:focus:text-white"></input>
+      <input type="submit" name="submit" value="submit" class="py-2 px-4 text-sm font-medium text-white bg-Glohaven-Orange rounded-lg border border-Glohaven-Orange hover:bg-Glohaven-Hovered hover:text-white focus:z-10 focus:ring-2 focus:ring-Glohaven-Orange focus:text-Glohaven-Orange"></input>
     </div>
 </form>
 @endsection

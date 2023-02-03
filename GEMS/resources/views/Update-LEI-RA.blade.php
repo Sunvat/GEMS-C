0ab236ff5eb07092940b5fc051b7ed51d86b0fc8
Hark25 <100450140+Hark25@users.noreply.github.com>
2023-02-03 11:56:41 -0800

    Revert "Exploration on main page features, and also some minor updates to LEI-Update page"

--- GEMS/resources/views/Update-LEI-RA.blade.php
+++ GEMS/resources/views/Update-LEI-RA.blade.php
@@ -10,28 +10,22 @@
 </div>
 
 <div class="container py-10 px-10 mx-0 min-w-full grid place-items-center" id = "container">
-  <form action="" method="get" id="updateForm">
-
-    <label for="rname" class="mx-2 font-bold text-slate-700">Location:</label><br>
-    <input type="text" id="rname" name="rname" placeholder="Okanagan" class="rounded p-2"><br>
-
-    <label for="country" class="mx-2 font-bold text-slate-700">Information/Update:</label><br>
-    <input type="text" id="country" name="country" placeholder="Canada" class="rounded p-2"><br>
-
-    <label for="provState" class="mx-2 font-bold text-slate-700">Datetime:</label><br>
-    <input type="text" id="provState" name="provState" placeholder="BC" class="rounded p-2"><br>
-    
-  <br>
-
-  <div class="md:flex md:items-center">
-      <div class="md:w-1/3">
-      </div>
-      <div class="md:w-2/3">
-        <input type="submit" name="submit" value="submit" class="py-2 px-4 text-sm font-medium text-white bg-Glohaven-Orange rounded-lg border border-Glohaven-Orange hover:bg-Glohaven-Hovered hover:text-white focus:z-10 focus:ring-2 focus:ring-Glohaven-Orange focus:text-Glohaven-Orange dark:bg-Glohaven-Orange dark:border-Glohaven-Hovered dark:text-white dark:hover:text-white dark:hover:bg-Glohaven-Hovered dark:focus:ring-blue-500 dark:focus:text-white"></input>
-      </div>
+<form action="" method="get" id="updateForm">
+  <label for="rname" class="mx-2 font-bold text-slate-700">Region Name:</label><br>
+  <input type="text" id="rname" name="rname" placeholder="Okanagan" class="rounded p-2"><br>
+  <label for="country" class="mx-2 font-bold text-slate-700">Country:</label><br>
+  <input type="text" id="country" name="country" placeholder="Canada" class="rounded p-2"><br>
+  <label for="provState" class="mx-2 font-bold text-slate-700">Province:</label><br>
+  <input type="text" id="provState" name="provState" placeholder="BC" class="rounded p-2"><br>
+ <br>
+ <div class="md:flex md:items-center">
+    <div class="md:w-1/3">
     </div>
-  </form>
-</div>
+    <div class="md:w-2/3">
+      <input type="submit" name="submit" value="submit" class="py-2 px-4 text-sm font-medium text-white bg-Glohaven-Orange rounded-lg border border-Glohaven-Orange hover:bg-Glohaven-Hovered hover:text-white focus:z-10 focus:ring-2 focus:ring-Glohaven-Orange focus:text-Glohaven-Orange dark:bg-Glohaven-Orange dark:border-Glohaven-Hovered dark:text-white dark:hover:text-white dark:hover:bg-Glohaven-Hovered dark:focus:ring-blue-500 dark:focus:text-white"></input>
+    </div>
+</form>
 
+</div>
 </body>
 @endsection

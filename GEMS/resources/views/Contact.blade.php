9b28c607b75cd52ad3c6f5ca4626470f4d3e5a20
Sunvat <sunvat14@gmail.com>
2023-02-03 15:06:19 -0800

    Works now

--- GEMS/resources/views/Contact.blade.php
+++ GEMS/resources/views/Contact.blade.php
@@ -5,11 +5,11 @@ this will update to display contact per region. sort of a call center list.
 <br>
 <div class = "flex flex-col justify-center items-center">
     
-    <div class = "my-4 py-14 bg-Lgreen rounded-lg">
-        <h1 class = "text-4xl">Contact Information:</h1>
-        <h2 class = "text-2xl"> region: okanagan: xxx-xxx-xxx</h2>
-        <h2 class = "text-2xl"> region: north vancouver: xxx-xxx-xxx</h2>
-        <h2 class = "text-2xl"> region: surrey: xxx-xxx-xxx</h2>
+    <div class = "my-4 py-14 bg-Mgreen rounded-lg">
+        <h1 class = "text-4xl text-white">Contact Information:</h1>
+        <h2 class = "text-2xl border-2 m-2 text-white"> Region: Okanagan: xxx-xxx-xxx</h2>
+        <h2 class = "text-2xl border-2 m-2 text-white"> Region: North Vancouver: xxx-xxx-xxx</h2>
+        <h2 class = "text-2xl border-2 m-2 text-white"> Region: Surrey: xxx-xxx-xxx</h2>
     </div>
 
 </div>
--- GEMS/resources/views/Create-Account.blade.php
+++ GEMS/resources/views/Create-Account.blade.php
@@ -6,65 +6,72 @@
 <body class="mx-auto">
 <div>
         
-    <?php  
-   echo "<div class=\" w-fit mx-auto px-52 py-24\" id = \"container\">";
-   echo " <form  method=\"get\" class=\"mx-auto\" action=\"\">";
-   echo "<label name =\"fname\" for=\"fname\" class=\"mx-2 font-bold text-white\">First Name:</label>";
-   echo " <br>";
-   echo "<input id=\"fname\" name =\"fname\" type=\"text\" placeholder=\"John\" class=\"mx-2 mb-4 py-2 px-2 border-2 rounded\">";
-   echo " <br>";
-   echo "<label name =\"lname\" for=\"lname\" class=\"mx-2 font-bold text-white\">Last Name:</label>";
-   echo " <br>";
-   echo "<input id=\"lname\" name =\"lname\" type=\"text\" placeholder=\"Doe\" class=\"mx-2 mb-4 py-2 px-2 border-2 rounded\">";
-   echo "<br>";
-   echo "<label name =\"email\" for=\"email\"class=\"mx-2 font-bold text-white\">E-mail:</label>";
-   echo "       <br>";
-   echo "       <input  id=\"email\" name =\"email\" type=\"email\" placeholder=\"john.doe@gmail.com\" class=\" mx-2 mb-4 py-2 px-2 border-2 rounded\">";
-   echo "       <br>";
-   echo "       <label  name =\"pword\" for=\"password\"class=\"mx-2 font-bold text-white\">Password:</label>";
-   echo "       <br>";
-   echo "       <input id=\"pword\" name =\"pword\" type=\"password\"   placeholder=\"Create Password\" class=\" mx-2 mb-4 py-2 px-2 border-2 rounded\">";
-   echo "       <p class = \"mx-2 text-xs text-white \">New password must contain:<br>";
+    <?php
+    if(isset($_GET['submit'])){
+    require dirname(__DIR__, 3).'/database/insertFuncs.php';
+
+    $newAccount = array(htmlentities($_GET['fname']),htmlentities($_GET['lname']),htmlentities($_GET['email']),htmlentities($_GET['pword']),htmlentities($_GET['id']),htmlentities($_GET['rID']));
+
+    InsertAccountInfo($newAccount);
+    header('Location: /Create-Account');
+    die();
+    }
+    ?>
+    
+    <div class=" w-fit mx-auto px-52 py-24" id = "container">
+        <form  method="get" class="mx-auto" action="" >
+                
+                <label name ="fname" for="fname" class="mx-2 font-bold text-white">First Name:</label>
+                <br>
+                <input id="fname" name ="fname" type="text" placeholder="John" class="mx-2 mb-4 py-2 px-2 border-2 rounded">
+                <br>
+                <label name ="lname" for="lname"class="mx-2 font-bold text-white">Last Name:</label>
+                <br>
+                <input id="lname" name ="lname" type="text" placeholder="Doe" class="mx-2 mb-4 py-2 px-2 border-2 rounded">
+                <br>
+                <label name ="email" for="email"class="mx-2 font-bold text-white">E-mail:</label>
+                <br>
+                <input  id="email" name ="email" type="email" placeholder="john.doe@gmail.com" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
+                <br>
+                <label  name ="pword" for="password"class="mx-2 font-bold text-white">Password:</label>
+                <br>
+                <input id="pword" name ="pword" type="password"   placeholder="Create Password" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
+                <p class = "mx-2 text-xs text-white ">New password must contain:<br>
+                <?php
                 if (isset($_GET['submit'])) {
                     $paword = $_GET['pword'];
                     $conpass = $_GET['confrim_pword'];
-                    if (strcmp($password, $conpass) != 0) {
+                    if (strcmp($pword, $conpass) != 0) {
                         echo "Invalid Password please use the criteria mentioned.";
                     } else {
-                        if (strlen($paword) < 8) {
+                        if (strlen($password) < 8) {
                             echo "Password must be at least 8 characters long";
-                        } elseif (!preg_match("#[a-z]+#", $password)) {
+                        } elseif (!preg_match("#[a-z]+#", $paword)) {
                             echo "Password must contain at least one lowercase letter";
-                        } elseif (!preg_match("#[A-Z]+#", $password)) {
+                        } elseif (!preg_match("#[A-Z]+#", $aword)) {
                             echo "Password must contain at least one uppercase letter";
-                        } elseif (!preg_match("#[0-9]+#", $password)) {
+                        } elseif (!preg_match("#[0-9]+#", $paword)) {
                             echo "Password must contain at least one number";
-                        } elseif (!preg_match("#[\W]+#", $password)) {
+                        } elseif (!preg_match("#[\W]+#", $paword)) {
                             echo "Password must contain at least one special character";
                     }
                 }
             }
-            else{
-                require dirname(__DIR__, 3).'/database/insertFuncs.php';
-                $newAccount = array(htmlentities($_GET['fname']),htmlentities($_GET['lname']),htmlentities($_GET['email']),htmlentities($_GET['pword']),htmlentities($_GET['id']),htmlentities($_GET['rID']));
-                InsertAccountInfo($newAccount);
-                header('Location: /Create-Account');
-                die();
-                }
-                echo "<ul class =\" mx-6 text-xs list-disc text-white\">";
-                echo " <li>At least 8 characters</li>";
-                echo " <li>At least 1 lowercase character</li>";
-                echo " <li>At least 1 upper character</li>";
-                echo "<li>At least 1 number</li>";
-                echo "<li>At least 1 special character</li>";
-                echo "</ul>";
-                echo "</p>";
-                echo "<br>";
-                echo "<label name =\"confrim_pword\" for=\"confirm_pass\"class=\"mx-2 font-bold text-white\">Confirm Password:</label>";
-                echo "<br>";
-                echo "<input id=\"confirm_pword\" name =\"confrim_pword\" type=\"password\" placeholder=\"Confirm Password\" class=\" mx-2 mb-4 py-2 px-2 border-2 rounded\">";
-                echo "<br>";
                 ?>
+                <ul class = " mx-6 text-xs list-disc text-white">
+                    <li>At least 8 characters</li>
+                    <li>At least 1 lowercase character</li>
+                    <li>At least 1 upper character</li>
+                    <li>At least 1 number</li>
+                    <li>At least 1 special character</li>
+                </ul>
+                </p>
+                <br>
+                <label name ="confrim_pword" for="confirm_pass"class="mx-2 font-bold text-white">Confirm Password:</label>
+                <br>
+                <input id="confirm_pword" name ="confrim_pword" type="password" placeholder="Confirm Password" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
+                <br>
+                
                 <label  name="id" for="I.D Number"class="mx-2 font-bold text-white">I.D Number:</label>
                 <br>
                 <input id="id" name="id" type="text" placeholder="I.D Number" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">

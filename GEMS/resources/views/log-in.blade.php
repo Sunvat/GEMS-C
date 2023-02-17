9e1b3c6c94b5fb49a85099db6176e60baf45acf5
Sunvat <sunvat14@gmail.com>
2023-02-17 13:51:51 -0800

    login page checks for null values

--- GEMS/resources/views/log-in.blade.php
+++ GEMS/resources/views/log-in.blade.php
@@ -6,21 +6,23 @@
 </div>
 <div class = "flex flex-col items-center">
     <div>
-        <form action="login.php" method = post id="loginForm"> <!-- dont forget to add a post or someother method when needed -->
+        <form action="login.php" method = post id="loginForm" onSubmit ="return validate();"> <!-- dont forget to add a post or someother method when needed -->
             <!-- used to create the login id and password slots-->
             <div class = "py-2">
                 <label class = "px-2 font-bold text-white">Login ID</label>
-                <input type="text" id = "id" name = "id" class="px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" required ><span id="user_info"></span><br>  
+                <input type="text" id = "id" name = "id" class="px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" required >
+                <span class="font-bold text-red-500" id="user_info"></span><br>  
             </div>
             <div>
                 <label for="Password" class = "px-2 font-bold text-white ">Password</label>
-                <input type="password" id = "pword" name = "id" class = "px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" required ><br><span id="pass_info"></span>
+                <input type="password" id = "pword" name = "id" class = "px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" required ><br>
+                <span class="font-bold text-red-500" id="pass_info"></span>
             </div class = "py-2">
             <br>
             <div class = "flex flex-col justify-center items-center">
-                <button class=" px-4 font-bold text-Dgreen bg-Lgreen rounded-full hover:bg-gold">
-                    <a href ="{{route('main-RA')}}" >Login</a>
-                </button><br>
+                <input class=" px-4 font-bold text-Dgreen bg-Lgreen rounded-full hover:bg-gold" type="submit" onClick= "return validate()" value="Login">
+                </input>
+                <br>
             </div>
         </form>
     </div>
@@ -29,15 +31,15 @@
             <h1 class = "text-lg font-semibold text-white">Don't have an account yet. No worries, create an account here:</h1>
         </div>
         <div>
-            <button>
+           <intput >
                 <a href="{{route('Create-Account')}}" class ="display: block  font-bold text-Dgreen bg-Lgreen rounded-full hover:bg-gold">Create Account</a>
-            </button>
+            </input>
         </div>
     </div>
 </div>
 <script>
     function validate() {
-        var $valid = true;
+        var valid = true;
         document.getElementById("user_info").innerHTML = "";
         document.getElementById("pass_info").innerHTML = "";
         
@@ -45,15 +47,15 @@
         var password = document.getElementById("pword").value;
         if(userName == "") 
         {
-            document.getElementById("user_info").innerHTML = "required";
-        	$valid = false;
+            document.getElementById("user_info").innerHTML = "User ID is Required to login.";
+        	valid = false;
         }
         if(password == "") 
         {
-        	document.getElementById("pass_info").innerHTML = "required";
-            $valid = false;
+        	document.getElementById("pass_info").innerHTML = "Password is required to login";
+            valid = false;
         }
-        return $valid;
+        return valid;
     }
     </script>
 @endsection

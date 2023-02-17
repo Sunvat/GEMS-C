9e535fa0f4931d8b695dc0d8d5a21efff69c7b51
Sunvat <sunvat14@gmail.com>
2023-02-17 11:29:38 -0800

    small changes on the log in page

--- GEMS/resources/views/log-in.blade.php
+++ GEMS/resources/views/log-in.blade.php
@@ -6,15 +6,15 @@
 </div>
 <div class = "flex flex-col items-center">
     <div>
-        <form action="" method = get> <!-- dont forget to add a post or someother method when needed -->
+        <form action="login.php" method = post id="loginForm"> <!-- dont forget to add a post or someother method when needed -->
             <!-- used to create the login id and password slots-->
             <div class = "py-2">
                 <label class = "px-2 font-bold text-white">Login ID</label>
-                <input type="text" id = "id" name = "id" class="px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"><br>  
+                <input type="text" id = "id" name = "id" class="px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" required ><span id="user_info"></span><br>  
             </div>
             <div>
                 <label for="Password" class = "px-2 font-bold text-white ">Password</label>
-                <input type="password" id = "pword" name = "id" class = "px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full"><br>
+                <input type="password" id = "pword" name = "id" class = "px-3 py-2 placeholder-slate-300 text-slate-600 relative bg-white bg-white rounded text-base border-0 shadow outline-none focus:outline-none focus:ring w-full" required ><br><span id="pass_info"></span>
             </div class = "py-2">
             <br>
             <div class = "flex flex-col justify-center items-center">
@@ -35,5 +35,25 @@
         </div>
     </div>
 </div>
-
+<script>
+    function validate() {
+        var $valid = true;
+        document.getElementById("user_info").innerHTML = "";
+        document.getElementById("pass_info").innerHTML = "";
+        
+        var userName = document.getElementById("id").value;
+        var password = document.getElementById("pword").value;
+        if(userName == "") 
+        {
+            document.getElementById("user_info").innerHTML = "required";
+        	$valid = false;
+        }
+        if(password == "") 
+        {
+        	document.getElementById("pass_info").innerHTML = "required";
+            $valid = false;
+        }
+        return $valid;
+    }
+    </script>
 @endsection

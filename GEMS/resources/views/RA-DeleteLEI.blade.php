0bda32789e9ee6eaf0624a8f115423900ad0f489
Hark25 <harklit25@gmail.com>
2023-02-17 11:07:17 -0800

    exploring javascript

--- GEMS/resources/views/RA-DeleteLEI.blade.php
+++ GEMS/resources/views/RA-DeleteLEI.blade.php
@@ -8,6 +8,7 @@
 
 <body>
 <?php
+  // deletes lei and returns back to main page.
   require dirname(__DIR__, 3).'/database/deleteFuncs.php';
 
   delLEI($_GET["rID"], $_GET("LEI"), $_get("Datetime"));

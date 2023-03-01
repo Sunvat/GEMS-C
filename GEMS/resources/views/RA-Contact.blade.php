a54443137552bb167da6263e8a4e1f03ea52dc1f
Hark25 <harklit25@gmail.com>
2023-03-01 10:34:26 -0800

    basic layout of html

--- GEMS/resources/views/RA-Contact.blade.php
+++ GEMS/resources/views/RA-Contact.blade.php
@@ -3,6 +3,10 @@
 @section('main')
 <br>
 <div class = "flex flex-col justify-center items-center">
+    
+    <div>
+        <button><a href="">Update Contact</a></button>
+    </div>
 
     <div class = "pt-14 mt-4 bg-Lgreen rounded-lg">
         <h1 class = "text-2xl text-white">Contact Information:</h1>
@@ -10,9 +14,8 @@
 
     <div>
         <table>
-
+            
         </table>
     </div>
-    
 </div>
 @endsection

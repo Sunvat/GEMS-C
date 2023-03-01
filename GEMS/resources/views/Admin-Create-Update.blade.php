8659aa34807adabe665c155d55a3ea9c1f85ee23
Hark25 <harklit25@gmail.com>
2023-03-01 10:34:28 -0800

    basic layout

---
+++ GEMS/resources/views/Admin-Create-Update.blade.php
@@ -0,0 +1,30 @@
+@extends('Layouts.RA-default')
+
+@section('main')
+
+<br>
+<div class = "flex flex-col justify-center items-center">
+    
+    <div>
+      <h1>Update Contact</h1>
+    </div>
+
+    <div> 
+      <form action="">
+
+      </form>
+    </div>
+  
+    <div>
+      <h1>Create New Contact</h1>
+    </div>
+
+    <div>
+      <form action="">
+        
+      </form>
+    </div>
+
+</div>
+
+@endsection

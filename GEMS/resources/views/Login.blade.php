b7690ee2c796ff1814c3af1aea8fef6dca20a4b5
Sunvat <sunvat14@gmail.com>
2023-03-08 14:01:44 -0800

    css changes

--- GEMS/resources/views/Login.blade.php
+++ GEMS/resources/views/Login.blade.php
@@ -1,9 +1,9 @@
 @extends('Layouts.default')
 @section('main')
 <body>
-    <h1 class="font-semibold text-2xl flex flex-col justify-center items-center pb-5">Log in:</h1>
-    <p class=" font-semibold text-xl flex flex-col justify-center items-center pb-5">Welcome back, please enter your detials</p>
-    <div class="flex flex-col justify-center items-center rounded-md ">
+    <h1 class="font-semibold text-2xl flex flex-col justify-center items-center pb-5 text-white">Log in:</h1>
+    <p class=" font-semibold text-xl flex flex-col justify-center items-center pb-5 text-white">Welcome back, please enter your detials</p>
+    <div class="flex flex-col justify-center items-center rounded-md  drop-shadow-2xl">
      @if (session('status'))
         <div class="success">
         {{ session('status') }}
@@ -20,7 +20,7 @@
      @endif
 
 
-        <form action="Login" method = post class="flex flex-col justify-center items-center bg-white w-96 h-64 rounded-md ">
+        <form action="Login" method = post class="flex flex-col justify-center items-center bg-white w-96 h-72 rounded-md ">
         <div class=" pb-5 font-semibold text-medium "><a class=" text-red-500 ">*</a>are required fields</div>
         @csrf
         <label class="pb-2 font-semibold text-medium" for="workID"><a class="font-bold text-red-500">*</a>ID:</label>
@@ -35,7 +35,7 @@
         @error('password')
                 <div class="danger font-bold text-red-500">{{ $message }}</div>
             @enderror
-        <button type="submit" class="mt-5 border-2 rounded-md p-1">Log In</button>
+        <button type="submit" class="mt-5 border-1 border-white rounded-md px-5 bg-red-500 transition ease-in-out delay-150 bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300">Log In</button>
         
     </form>
     </div>

5b6422b6eff244934f540e8b6a9ded9e197256a8
Mack S <60047062+Time-Vault@users.noreply.github.com>
2023-03-14 22:51:32 -0700

    Fixed font colours

--- GEMS/resources/views/Create-Account.blade.php
+++ GEMS/resources/views/Create-Account.blade.php
@@ -66,26 +66,26 @@
     <div class=" w-fit mx-auto px-52 py-14" id = "container">
         <form class="flex-items-center" method="get" class="mx-auto" action="Create-Account" >
                 @csrf
-                <label name ="fname" for="fname" class="mx-2 font-bold text-white">First Name:<a class="text-red-500"> *</a></label>
+                <label name ="fname" for="fname" class="mx-2 font-bold text-black">First Name:<a class="text-red-500"> *</a></label>
                 <br>
                 <input id="fname" name ="fname" type="text" required placeholder="John" class="mx-2 mb-4 py-2 px-2 border-2 rounded">
                 <br>
-                <label name ="lname" for="lname"class="mx-2 font-bold text-white">Last Name:<a class="text-red-500"> *</a></label>
+                <label name ="lname" for="lname"class="mx-2 font-bold text-black">Last Name:<a class="text-red-500"> *</a></label>
                 <br>
                 <input id="lname" name ="lname" type="text" required placeholder="Doe" class="mx-2 mb-4 py-2 px-2 border-2 rounded">
                 <br>
-                <label name ="email" for="email"class="mx-2  font-bold text-white">E-mail:<a class="text-red-500"> *</a></label>
+                <label name ="email" for="email"class="mx-2  font-bold text-black">E-mail:<a class="text-red-500"> *</a></label>
                 <br>
                 <input  id="email" name ="email" type="email" required placeholder="john.doe@gmail.com" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
                 <br>
-                <label  name ="pword" for="password"class="mx-2 font-bold text-white">Password:<a class="text-red-500"> *</a></label>
+                <label  name ="pword" for="password"class="mx-2 font-bold text-black">Password:<a class="text-red-500"> *</a></label>
                 <br>
                 <input id="pword" name ="pword" type="password"  required    placeholder="Create Password" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
-                <p class = "mx-2 text-xs text-white ">New password must contain:<br>
+                <p class = "mx-2 text-xs text-black ">New password must contain:<br>
                 <?php
               
                 ?>
-                <ul class = " mx-6 text-xs list-disc text-white">
+                <ul class = " mx-6 text-xs list-disc text-black">
                     <li id="minChar">At least 8 characters</li>
                     <li id="lowChar">At least 1 lowercase character</li>
                     <li id="upChar">At least 1 upper character</li>
@@ -94,12 +94,12 @@
                 </ul>
                 </p>
                 <br>
-                <label name ="confrim_pword" for="confirm_pass"class="mx-2 font-bold text-white">Confirm Password:<a class="text-red-500"> *</a></label>
+                <label name ="confrim_pword" for="confirm_pass"class="mx-2 font-bold text-black">Confirm Password:<a class="text-red-500"> *</a></label>
                 <br>
                 <input id="confirm_pword" name ="confrim_pword" type="password" required placeholder="Confirm Password" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
                 <br>
                 
-                <label  name="id" for="I.D Number"class="mx-2 font-bold text-white">I.D Number:<a class="text-red-500"> *</a></label>
+                <label  name="id" for="I.D Number"class="mx-2 font-bold text-black">I.D Number:<a class="text-red-500"> *</a></label>
                 <br>
                 <input id="id" name="id" type="text" placeholder="I.D Number" required class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
                 <br>
@@ -110,7 +110,7 @@
 
     echo "<label for=\"chooseRegion\" class=\"block mb-2 text-large font-medium text-gray-900 dark:text-grey\">Select a region to browse available accommodations in that region:</label>";
     
-    echo "<select name = \"rID\" id=\"rID\" class=\"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500\">";
+    echo "<select name = \"rID\" id=\"rID\" class=\"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500\">";
 
     while($row = mysqli_fetch_array($result)){
       echo "<option value =" . htmlspecialchars($row['rID'], ENT_QUOTES, 'UTF-8') . ">" . htmlspecialchars($row['rID'], ENT_QUOTES, 'UTF-8') .": ". htmlspecialchars($row['rname'], ENT_QUOTES, 'UTF-8') . "</option>";
@@ -120,7 +120,7 @@
     echo "<div class = \"flex flex-col jusify-center items-center\">";
     echo "</div>";
     ?>
-                <label for="Confirmation Code"class="mx-2 font-bold text-white">Confirmation Code: (Emailed to new assignee when new RA is assigned)</label>
+                <label for="Confirmation Code"class="mx-2 font-bold text-black">Confirmation Code: (Emailed to new assignee when new RA is assigned)</label>
                 <br>
                 <input type="text" placeholder="Confirmation Code" class=" mx-2 mb-4 py-2 px-2 border-2 rounded">
                 <br>
@@ -128,7 +128,7 @@
                 <input onclick="" type='submit' name='submit' value='submit'></input>
                  </div>
         </form>
-        <p class="mt-10 font-bold text-white"><a class="font-bold text-red-500">*</a> Are Required fields</p>
+        <p class="mt-10 font-bold text-black"><a class="font-bold text-red-500">*</a> Are Required fields</p>
     </div>
  </div>
 

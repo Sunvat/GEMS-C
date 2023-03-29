b4dca7e478fd1fd1e4f9a33c31d8f93e46dcdb3f
Sunvat <sunvat14@gmail.com>
2023-03-29 10:20:37 -0700

    changed logo on both ra and main pages

--- GEMS/resources/views/Layouts/RA-default.blade.php
+++ GEMS/resources/views/Layouts/RA-default.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Glohaven Emergency Management System</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('/images/glohavenlogo.png') }}">
+       <link rel="icon" src="https://github.com/Sunvat/GEMS-C/blob/main/GEMS/resources/images/gemslogo.jpg">
     @vite('resources/css/app.css')
 </head>
 <body class = "float-none flex flex-col min-h-screen bg-orange bg-cover">
@@ -13,9 +13,9 @@
    <div class="h-full px-3 py-4 overflow-y-auto bg-white-10 border">
    <a href="{{route('Home')}}" class="flex items-center mb-4 sm:mb-0">
             <figure>
-            <img src = "{{ asset('/images/glohavenlogo.png') }}" class=""/>
+            <img src = "{{ asset('/images/GEMS.jpg') }}" class="w-40"/>
             </figure>
-            <span class="self-center text-3xl font-serif px-2 " style="color:#f6ad55">G.E.M.S</span>
+            
         </a>
       <ul class="space-y-2">
         <br>
